87857a2e937c9fae8d818202afd7e0e805b4769e
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-07-04 10:27:04 -0300

    Step 4 in Form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -7,6 +7,7 @@ import two from '../../images/two.png'
 import three from '../../images/three.png'
 import { Header } from "../Header/Header"
 import { Loading } from "../Loading/Loading"
+import { Card } from "../Home/Card/Card"
 
 export function Form(){
 
@@ -29,28 +30,29 @@ export function Form(){
     function handleValue(e){
         if(e.target.name === "type"){
                 if(e.target.checked){
-                    setValue({...value, Types: [...value.Types, e.target.value]})
+                    setValue({...value, Types: [...value.Types, {name: e.target.value}]})
                 }else{
                     //In the case they unchecked something
-                    let checkedTypes = (value.Types).filter(el => el !== e.target.value)
+                    let checkedTypes = (value.Types).filter(el => el.name !== e.target.value)
                     setValue({...value, Types: checkedTypes })
                 }
         }else{
             setValue({
                    ...value,
                    [e.target.name]: e.target.value
-               })   
+               })
         }
-
     }
     //continue coding the error and add to the handlevlaue
     function validate(value){
         let errors = {}
         if(value.Types.length <= 0) errors["type"] = "Please select at least one type"
+        else if (value.Types.length > 2) errors["type"] = "A pokemon can not have more than 2 types"
         if(!(value.img).includes("https://")) errors["url"] = "Please insert a correct URL"
         if(value.name){
             let alreadyExist = pokemons.filter(el=> el.name === value.name)
             if(alreadyExist.length) errors["name"] = "This name already exist"
+            if(value.name.length > 10) errors["name"] = "The name must not exceed 10 characters"
         }
         return errors
         
@@ -65,8 +67,8 @@ export function Form(){
             dispatch(postPokemon(value))
             setValue({
                 name:'', 
-                life: 0,
-                attack: 0,
+                life: 50,
+                attack: 50,
                 defense:50,
                 speed:50,
                 height:50,
@@ -103,30 +105,30 @@ export function Form(){
                             <div className={s.stats}>
                                 <div>
                                     <label htmlFor="life">Life:  <span>{value.life}</span> </label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="life"  min="1" max="150" value={value.life}/>
+                                    <input onChange={e=>handleValue(e)} type="range" name="life"  min="1" max="110" value={value.life}/>
                                 </div>
                                 <div>
                                     <label htmlFor="attack">Attack: <span>{value.attack}</span></label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="attack" min="1" max="150" value={value.attack} />
+                                    <input onChange={e=>handleValue(e)} type="range" name="attack" min="1" max="110" value={value.attack} />
                                 </div>
                                 <div>
                                     <label htmlFor="defense">Defense:  <span>{value.defense}</span></label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="defense"  min="1" max="150" value={value.defense} />
+                                    <input onChange={e=>handleValue(e)} type="range" name="defense"  min="1" max="110" value={value.defense} />
 
                                 </div>
                                 <div>
                                     <label htmlFor="speed">Speed: <span>{value.speed}</span></label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="speed" min="1" max="150" value={value.speed} />
+                                    <input onChange={e=>handleValue(e)} type="range" name="speed" min="1" max="110" value={value.speed} />
 
                                 </div>
                                 <div>
                                     <label htmlFor="height">Height:  <span>{value.height}</span></label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="height"  min="1" max="150" value={value.height} />
+                                    <input onChange={e=>handleValue(e)} type="range" name="height"  min="1" max="110" value={value.height} />
 
                                 </div>
                                 <div>
                                     <label htmlFor="weight">Weight:  <span>{value.weight}</span> </label>
-                                    <input onChange={e=>handleValue(e)} type="range" name="weight" min="1" max="150"  value={value.weight}/>
+                                    <input onChange={e=>handleValue(e)} type="range" name="weight" min="1" max="110"  value={value.weight}/>
                                 </div>
                             </div>
                         </div>
@@ -142,7 +144,19 @@ export function Form(){
                                     </div>
                                 ))}
                             </div>
-                        </div>       
+                        </div>
+                        <div>
+                                    <Card
+                                        name={value.name } 
+                                        life={value.life}
+                                        height={value.height}
+                                        weight= {value.weight}
+                                        attack= {value.attack}
+                                        speed= {value.speed}
+                                        defense= {value.defense }
+                                        type= {value.Types}
+                                        img= {value.img }/>
+                        </div>    
                     </div>     
                 <input className={s.formBtn} type="submit"  value="Create"/>
                 </form>
