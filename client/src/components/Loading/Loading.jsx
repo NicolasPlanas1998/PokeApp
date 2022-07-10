88f26e0284ebaafa8096d8e86dc27b8ee039aace
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-07-10 14:17:47 -0300

    Loading gif

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,12 +1,10 @@
 import pikachuLoading from "../../images/pikachuLoading.gif"
-import loadingCircle from "../../images/loadingCircle.gif"
 import s from "./loading.module.css"
 
 export function Loading (){
     return(
         <div className={s.container}>
             <img  src={pikachuLoading} alt="Loading" />
-            <img src={loadingCircle} id={s.circle} alt="Loading circle" />
         </div>
     )
 }
